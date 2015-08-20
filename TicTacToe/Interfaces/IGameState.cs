82ce33c4ee95cfaeadf6a919678fb4c8734b6480
Michael Dann <michael@Theseus.default>
2015-08-20 13:33:34 +0100

    initial add

---
+++ TicTacToe/Interfaces/IGameState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Interfaces
+{
+    public interface IGameState
+    {
+        public bool HasStarted();
+
+        public bool HasFinished();
+    }
+}
---
+++ TicTacToe/Interfaces/IPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Interfaces
+{
+    /// <summary>
+    /// Interface to be passed into a game as a player
+    /// </summary>
+    public interface IPlayer
+    {
+        public Point NextMove(IGameState gameState);
+    }
+}
